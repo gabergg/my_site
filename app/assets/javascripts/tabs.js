d94edfd38caa7694bed820f6446c93326b1fdd8c
Gabe G'Sell <gabergg@gmail.com>
2014-10-15 02:32:42 -0400

    working site w/ rails

---
+++ app/assets/javascripts/tabs.js
@@ -0,0 +1,15 @@
+$(document).ready( function(){
+    $('#biography_tab').show();
+    $('.selected').css("opacity","1");
+    $('a').on('click',function(){
+        $('.selected').css("opacity",".5");
+        $('.selected').removeClass('selected');
+        $(this).closest('li').addClass('selected');
+        $('.selected').css("opacity","1");
+
+        $('.content').hide();
+
+        var choose_content = $(this).attr('title');
+        $("#"+choose_content).show();
+    });
+});
