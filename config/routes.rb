65ffe7b0e021c43540ae4e2bb8acaf2533dcfbc1
Gabe G'Sell <gabergg@gmail.com>
2014-11-04 02:14:54 -0500

    header footer foundation, new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 MySite::Application.routes.draw do
   
   root 'static_pages#home'
+
+  match '/contact',    to: 'static_pages#contact', via: 'get'
+  match '/projects',     to: 'static_pages#projects', via: 'get'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
